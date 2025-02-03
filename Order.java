7c729b904d61f2dc504bb2acc128bbcc63d47e50
BimaBalaakosa <bimadrive7@gmail.com>
2025-02-03 09:24:13 +0800

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,32 +1,4 @@
 import java.util.Scanner;
-
-public class Order {
-    public void OrderCustomer (Scanner input) {
-    while (true) {
-      
-      int cake = input.nextInt();
-      switch (cake) {
-        case 1:
-        System.out.println();
-        break;
-        case 2:
-        System.out.println();
-        break;
-        case 3:
-        System.out.println();
-        break;
-        case 4:
-        System.out.println();
-        return;
-        default:
-        System.out.println("Invalid option");
-        return;
-      }
-    }
-  }
-
-
-    import java.util.Scanner;
 import java.time.LocalDate;
 public class Order {
   private int month;
