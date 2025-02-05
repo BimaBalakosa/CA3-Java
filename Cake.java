2baeb8bb049b46a67c8c66b03526acecbf91e72e
JerryTheTus <tranvuongminhtu@gmail.com>
2025-02-05 19:50:34 +0800

    Update Cake.java

--- Cake.java
+++ Cake.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import java.util.Arrays;
+
 public class Cake {
   private int cakeCode;
   private String cakeName;
@@ -28,84 +28,88 @@ public class Cake {
     return cakePrice;
   }
   public static String[][] CakeMenu (Scanner input, String[][] cake) {
-  while (true) {
-    System.out.println("\n***Cake Menu***\n1. Create Cake\n2. Update Cake\n3. Delete Cake\n4. View all Cakes\n5. Back to the Main Menu\n");
-    System.out.print("Enter option: ");
-    int option = input.nextInt();
-    switch (option) {
-      case 1: 
-        System.out.print("\nEnter Cake's Name:");
-        String newCakeName = input.nextLine();
-        input.nextLine();
-        System.out.print("\nEnter Cake Code: ");
-        int newCakeCode = input.nextInt();  
-        input.nextLine();  
-        System.out.print("\nEnter Cake Price: ");
-        double newCakePrice = input.nextDouble();  
-        input.nextLine(); 
-        
-        String[][] newCake = new String[cake.length+1][];
-        System.arraycopy(cake,0,newCake,0, cake.length);
-        newCake [cake.length] = new String[]{String.valueOf(newCakeCode), newCakeName, String.valueOf(newCakePrice)};
-        cake = newCake;
-        break;
-      case 2:
-        System.out.print("Enter Cake Code to update: ");
-        int updateCakeCode = input.nextInt();
-        if (checkCakeIDExistence(cake, updateCakeCode) == false){
-          System.out.print("\nError: No cake code found");
+    while (true) {
+      System.out.println("\n***Cake Menu***\n1. Create Cake\n2. Update Cake\n3. Delete Cake\n4. View all Cakes\n5. Back to the Main Menu\n");
+      System.out.print("Enter option: ");
+      int option = input.nextInt();
+      switch (option) {
+        case 1: 
+          System.out.print("\nEnter Cake's Name:");
+          String newCakeName = input.nextLine();
+          input.nextLine();
+          System.out.print("\nEnter Cake Code: ");
+          int newCakeCode = input.nextInt();  
+          input.nextLine();  
+          System.out.print("\nEnter Cake Price: ");
+          double newCakePrice = input.nextDouble();  
+          input.nextLine(); 
+          
+          String[][] newCake = new String[cake.length+1][];
+          System.arraycopy(cake,0,newCake,0, cake.length);
+          newCake [cake.length] = new String[]{String.valueOf(newCakeCode), newCakeName, String.valueOf(newCakePrice)};
+          cake = newCake;
           break;
-        }
-        else{
-          for (int i = 0; i<cake.length; i++){
-            if (Integer.parseInt(cake[i][0]) == updateCakeCode){
-              System.out.println("Error: Cake Code already exists");
+        case 2:
+          System.out.print("Enter Cake Code to update: ");
+          int updateCakeCode = input.nextInt();
+          if (checkCakeIDExistence(cake, updateCakeCode) == false){
+            System.out.print("\nError: No cake code found");
+            break;
+          }
+          else{
+            for (int i = 0; i<cake.length; i++){
+              if (Integer.parseInt(cake[i][0]) == updateCakeCode){
+                System.out.println("Error: Cake Code already exists");
+              }
+              else{
+                System.out.print("Enter new Cake Code: ");
+                int updateCode = input.nextInt();
+                input.nextLine();
+                System.out.print("\nUpdate Cake name: ");
+                String updateName = input.nextLine();
+                System.out.print("\nUpdate Cake price: ");
+                double updatePrice = input.nextDouble();
+                input.nextLine();
+                cake [i] = new String[]{String.valueOf(updateCode), updateName, String.valueOf(updatePrice)};
+              }
+            } 
+          }
+          break;
+        case 3:
+          System.out.println("Enter Cake Code to delete: ");
+          int deleteCakeCode = input.nextInt();
+          input.nextLine();
+          if (checkCakeIDExistence(cake, deleteCakeCode)) {
+            String[][] newCakeArray = new String[cake.length - 1][];
+            int index = 0;
+            for (int i = 0; i < cake.length; i++) {
+              if (Integer.parseInt(cake[i][0]) != deleteCakeCode) {
+                newCakeArray[index++] = cake[i];
+              }
             }
-            else{
-              System.out.print("Enter new Cake Code: ");
-              int updateCode = input.nextInt();
-              input.nextLine();
-              System.out.print("\nUpdate Cake name: ");
-              String updateName = input.nextLine();
-              System.out.print("\nUpdate Cake price: ");
-              double updatePrice = input.nextDouble();
-              input.nextLine();
-              cake [i] = new String[]{String.valueOf(updateCode), updateName, String.valueOf(updatePrice)};
-            }
-          } 
-        }
-        break;
-      case 3:
-        System.out.println("Enter Cake Code to delete: ");
-        int deleteCakeCode = input.nextInt();
-        input.nextLine();
-        if (checkCakeIDExistence(cake, deleteCakeCode)) {
-          String[][] newCakeArray = new String[cake.length - 1][];
-          int index = 0;
-          for (int i = 0; i < cake.length; i++) {
-            if (Integer.parseInt(cake[i][0]) != deleteCakeCode) {
-              newCakeArray[index++] = cake[i];
+            cake = newCakeArray;
+            System.out.printf("Cake %d is removed", deleteCakeCode);
+          } else {
+            System.out.println("Error: No cake code found");
+          }
+          break;
+        case 4:
+          if (cake.length == 0){
+            System.out.println("Out of Cake");
+          }
+          else{         
+            System.out.println("All Cakes: ");
+            for (int i = 0; i < cake.length; i++) {
+              System.out.println("Cake Code: " + cake[i][0] + ", Name: " + cake[i][1] + ", Price: " + cake[i][2]);
             }
           }
-          cake = newCakeArray;
-          System.out.printf("Cake %d is removed", deleteCakeCode);
-        } else {
-          System.out.println("Error: No cake code found");
-        }
-        break;
-      case 4:
-        System.out.println("All Cakes: ");
-        
-        for (int i = 0; i < cake.length; i++) {
-          System.out.println("Cake Code: " + cake[i][0] + ", Name: " + cake[i][1] + ", Price: " + cake[i][2]);
-        }
-        break;
-      case 5:
-        System.out.println("Returning to the main menu.");
-        return cake;
-      default:
-        System.out.println("Invalid option");
-        break;
+          break;
+        case 5:
+          System.out.println("Returning to the main menu.");
+          return cake;
+        default:
+          System.out.println("Invalid option");
+          break;
       }
     }
   }
@@ -118,5 +122,4 @@ public class Cake {
     return false;
   }
 }
-
 
